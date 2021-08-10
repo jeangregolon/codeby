8343fb7443d5622c4ec2495bbb8f910ec442f8e7
unknown <jean.gregolon@gmail.com>
2021-08-10 20:16:01 -0300

    Added Link to Home Screen buttons

--- src/screens/HomeScreen/HomeScreen.jsx
+++ src/screens/HomeScreen/HomeScreen.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import './homeScreen.scss'
 
 export default function HomeScreen(){
     return(
         <div className='row'>
-            <button>Carrinho 1 <div>Sem frete</div></button>
-            <button>Carrinho 2 <div>Com frete</div></button>
+            <Link to='/carrinho' params={{ data: "sem-frete" }}>
+                <button>Carrinho 1 <div>Sem frete</div></button>
+            </Link>
+            <Link to='/carrinho' params={{ data: "com-frete" }}>
+                <button>Carrinho 2<div>Com frete</div></button>
+            </Link>
         </div>
     )
 }
