7bff0ae8c5c738b189160def02b33275b4b5c468
unknown <jean.gregolon@gmail.com>
2021-08-10 17:16:43 -0300

    Created header, added node-sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen';
-
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <main>
-      <Route path='/' component={HomeScreen} exact />
+        <Route path='/' component={HomeScreen} exact />
       </main>
     </BrowserRouter>
   );
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './header.scss'
+
+export default function Header() {
+return(
+    <header>
+      <a href="/"><img src="./assets/logo-codeby.png" alt="Logo Codeby"/></a>
+    </header>
+)
+}
