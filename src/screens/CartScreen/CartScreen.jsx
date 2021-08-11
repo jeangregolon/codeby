9f59327e9d02d80ff5494cbc858ed1af64765040
unknown <jean.gregolon@gmail.com>
2021-08-10 22:17:25 -0300

    Updated Cart Screen

--- src/screens/CartScreen/CartScreen.jsx
+++ src/screens/CartScreen/CartScreen.jsx
@@ -3,9 +3,30 @@ import {Link} from 'react-router-dom'
 import './cartScreen.scss'
 
 export default function CartScreen(){
+  const totalPrice = 11
+
   return(
-    <div >
-      <h1>Meu carrinho</h1>
+    <div className='container'>
+      <h1 className='title'>Meu carrinho</h1>
+      <h3>Card</h3>
+      <h3>Card</h3>
+      <h3>Card</h3>
+      <h3>Card</h3>
+      <div className='total-n-shipping'>
+        <div className='total-price'>
+          <span>Total</span>
+          <span>R$9,99</span>
+        </div>
+        <div>
+        {totalPrice > 10 ? 
+          (
+            <div className='free-shipping'>Parabéns, sua compra tem frete grátis!</div>
+            ):(
+              ""
+              )}
+        </div>
+      </div>
+      <button className='finish-btn'>Finalizar compra</button>
     </div>
   )
 }
