cb4df751b0ec58876bf5a2736a4892c7510188a2
unknown <jean.gregolon@gmail.com>
2021-08-10 21:00:15 -0300

    SCSS corrections

--- src/screens/HomeScreen/HomeScreen.jsx
+++ src/screens/HomeScreen/HomeScreen.jsx
@@ -6,10 +6,10 @@ export default function HomeScreen(){
   return(
     <div className='row'>
       <Link to='/carrinho' params={{ data: "sem-frete" }}>
-        <button>Carrinho 1 <div>Sem frete</div></button>
+        <button className='cart-button'>Carrinho 1 <div>Sem frete</div></button>
       </Link>
       <Link to='/carrinho' params={{ data: "com-frete" }}>
-        <button>Carrinho 2<div>Com frete</div></button>
+        <button className='cart-button'>Carrinho 2<div>Com frete</div></button>
       </Link>
     </div>
   )
